4b9e049449fe7d4e1c7da5213a9ce6494ebe6371
Igor Francesco <igorfrancesco2@gmail.com>
2024-07-04 17:45:38 -0300

    update repository/

--- repository/trade_repository.go
+++ repository/trade_repository.go
@@ -1,35 +1,47 @@
 package repository
 
 import (
-	"database/sql"
+	"github.com/igorfarodrigues/api-cotacoes-b3/db"
 	"github.com/igorfarodrigues/api-cotacoes-b3/models"
 	_ "github.com/lib/pq"
 )
 
-type TradeRepository struct {
-	DB *sql.DB
+func SaveTrade(trade *models.Trade) (id int64, err error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+
+	defer conn.Close()
+
+	query := `INSERT INTO trades (hora_fechamento, data_negocio, codigo_instrumento, preco_negocio, quantidade_negociada) VALUES ($1, $2, $3, $4, $5) RETURNING id`
+	err = conn.QueryRow(query, trade.HoraFechamento, trade.DataNegocio, trade.CodigoInstrumento, trade.PrecoNegocio, trade.QuantidadeNegociada).Scan(&id)
+	return
 }
 
-func NewTradeRepository(db *sql.DB) *TradeRepository {
-	return &TradeRepository{DB: db}
-}
+func GetMaxRangeValue(ticker string, date string) (maxRangeValue float64, err error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return maxRangeValue, err
+	}
 
-func (repo *TradeRepository) SaveTrade(trade *models.Trade) error {
-	query := `INSERT INTO trades (hora_fechamento, data_negocio, codigo_instrumento, preco_negocio, quantidade_negociada) VALUES ($1, $2, $3, $4, $5)`
-	_, err := repo.DB.Exec(query, trade.HoraFechamento, trade.DataNegocio, trade.CodigoInstrumento, trade.PrecoNegocio, trade.QuantidadeNegociada)
-	return err
-}
+	defer conn.Close()
 
-func (repo *TradeRepository) GetMaxRangeValue(ticker string, date string) (float64, error) {
-	var maxRangeValue float64
 	query := `SELECT MAX(preco_negocio) FROM trades WHERE codigo_instrumento = $1 AND data_negocio >= $2`
-	err := repo.DB.QueryRow(query, ticker, date).Scan(&maxRangeValue)
+	err = conn.QueryRow(query, ticker, date).Scan(&maxRangeValue)
 	return maxRangeValue, err
 }
 
-func (repo *TradeRepository) GetMaxDailyVolume(ticker string, date string) (int, error) {
-	var maxDailyVolume int
+func GetMaxDailyVolume(ticker string, date string) (maxDailyVolume int, err error) {
+
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return maxDailyVolume, err
+	}
+
+	defer conn.Close()
+
 	query := `SELECT MAX(total_volume) FROM (SELECT SUM(quantidade_negociada) as total_volume FROM trades WHERE codigo_instrumento = $1 AND data_negocio >= $2 GROUP BY data_negocio) as subquery`
-	err := repo.DB.QueryRow(query, ticker, date).Scan(&maxDailyVolume)
+	err = conn.QueryRow(query, ticker, date).Scan(&maxDailyVolume)
 	return maxDailyVolume, err
 }
