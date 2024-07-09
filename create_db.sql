09d055ba92ad804e68d1920542cf39196e99cc57
Igor Francesco <igorfrancesco2@gmail.com>
2024-07-09 14:36:09 -0300

    update script sql

--- create_db.sql
+++ create_db.sql
@@ -4,11 +4,29 @@ CREATE DATABASE api_cotacao;
 
 \c api_cotacao
 
-CREATE TABLE IF NOT EXISTS trades (
+BEGIN;
+
+-- Verificação de existência e criação das tabelas
+DROP TABLE IF EXISTS trades;
+DROP TABLE IF EXISTS tickers;
+
+CREATE TABLE tickers (
     id SERIAL PRIMARY KEY,
-    hora_fechamento VARCHAR(500),
-    data_negocio DATE,
-    codigo_instrumento VARCHAR(300),
-    preco_negocio DECIMAL(10, 2),
-    quantidade_negociada INT  
+    codigo_instrumento VARCHAR(100) UNIQUE NOT NULL
 );
+
+CREATE TABLE trades (
+    id SERIAL PRIMARY KEY,
+    hora_fechamento VARCHAR(100),
+    data_negocio DATE,
+    codigo_instrumento VARCHAR(100) NOT NULL,
+    preco_negocio DECIMAL(10, 2),
+    quantidade_negociada INT,
+    FOREIGN KEY (codigo_instrumento) REFERENCES tickers (codigo_instrumento)
+);
+
+-- Criação de índices
+DROP INDEX IF EXISTS idx_ticker_date;
+CREATE INDEX idx_ticker_date ON trades (codigo_instrumento, data_negocio);
+
+COMMIT;
