5a897e487f667375c95f852dba48561148325519
Igor Francesco <igorfrancesco2@gmail.com>
2024-07-09 14:37:07 -0300

    improve sql insert

--- loadFiles/repository/insert_load_files_repository.go
+++ loadFiles/repository/insert_load_files_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/igorfarodrigues/api-cotacoes-b3/db"
@@ -16,10 +17,28 @@ func SaveTrade(trade *models.Trade) (id int64, err error) {
 
 	defer conn.Close()
 
+
+	// Verifique se o codigo_instrumento existe
+	var exists bool
+	err = conn.QueryRow("SELECT EXISTS (SELECT 1 FROM tickers WHERE codigo_instrumento = $1)", trade.CodigoInstrumento).Scan(&exists)
+	if err != nil {
+		return 0, fmt.Errorf("erro ao verificar existencia de codigo_instrumento: %v", err)
+	}
+
+	// Se não existir, insira na tabela de tickers
+	if !exists {
+		_, err := conn.Exec("INSERT INTO tickers (codigo_instrumento) VALUES ($1)", trade.CodigoInstrumento)
+		if err != nil {
+			return 0, fmt.Errorf("erro ao inserir novo codigo_instrumento: %v", err)
+		}
+	}
+
+
+
 	query := `INSERT INTO trades (hora_fechamento, data_negocio, codigo_instrumento, preco_negocio, quantidade_negociada) VALUES ($1, $2, $3, $4, $5) RETURNING id`
 
 	if err = conn.QueryRow(query, trade.HoraFechamento, trade.DataNegocio, trade.CodigoInstrumento, trade.PrecoNegocio, trade.QuantidadeNegociada).Scan(&id); err != nil {
 		log.Printf("Erro ao persistir os dados na tabela trades %d: %v", id, err)
 	}
-	return
+	return id, nil
 }
